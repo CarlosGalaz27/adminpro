11201ab897fdbed978523416720c7b9b32a8a8d9
Carlos Galaz <carlgalaz.roman@gmail.com>
2018-11-22 18:02:20 -0300

    seccion 4 terminada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,14 +10,6 @@ import { ProgressComponent } from './pages/progress/progress.component';
 import { RegisterComponent } from './login/register.component';
 
 const app_route : Routes = [
-	{	path: '',
-		component: PagesComponent,
-		children: [
-			{path: 'dashboard', component : DashboardComponent },
-			{path: 'graficas1', component : Graficas1Component },
-			{path: 'progress', component : ProgressComponent },
-			{path: '', redirectTo : '/dashboard', pathMatch : 'full'},
-		]},
 	{path: 'login', component : LoginComponent },
 	{path: 'register', component : RegisterComponent },
 	{path: '**', component : NopagefoundComponent }
